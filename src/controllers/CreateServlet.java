4106cbc4c610850da4685bd91db9c982ed8d7280
momoko.shiozaki <zakimomo01@gmail.com>
2023-01-09 20:19:14 +0900

    フラッシュメッセージを追加

--- src/controllers/CreateServlet.java
+++ src/controllers/CreateServlet.java
@@ -50,6 +50,11 @@ public class CreateServlet extends HttpServlet {
 
             em.persist(m);
             em.getTransaction().commit();
+            // データベースに保存
+            em.persist(m);
+            em.getTransaction().commit();
+            request.getSession().setAttribute("flush", "登録が完了しました。");       // ここを追記
+
             em.close();
 
             response.sendRedirect(request.getContextPath() + "/index");
--- src/controllers/DestroyServlet.java
+++ src/controllers/DestroyServlet.java
@@ -38,10 +38,13 @@ public class DestroyServlet extends HttpServlet {
             // 該当のIDのメッセージ1件のみをデータベースから取得
             Message m = em.find(Message.class, (Integer)(request.getSession().getAttribute("message_id")));
 
+
             em.getTransaction().begin();
             em.remove(m);       // データ削除
             em.getTransaction().commit();
+            request.getSession().setAttribute("flush", "削除が完了しました。");       // ここを追記
             em.close();
+
 
             // セッションスコープ上の不要になったデータを削除
             request.getSession().removeAttribute("message_id");
