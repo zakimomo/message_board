4544fe0d414b645132e84f16f3e3f92a4c0cbfda
momoko.shiozaki <zakimomo01@gmail.com>
2023-01-09 20:29:52 +0900

    ページネーションを追加

--- src/models/Message.java
+++ src/models/Message.java
@@ -1,22 +1,26 @@
 package models;
 
 import java.sql.Timestamp;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 @Entity
 @NamedQueries({
     @NamedQuery(
-        name = "getAllMessages",
-        query = "SELECT m FROM Message AS m ORDER BY m.id DESC"
-    )
+            name = "getAllMessages",
+            query = "SELECT m FROM Message AS m ORDER BY m.id DESC"
+            ),
+    @NamedQuery(
+            name = "getMessagesCount",
+            query = "SELECT COUNT(m) FROM Message AS m"
+            )
 })
 
 @Table(name = "messages")
